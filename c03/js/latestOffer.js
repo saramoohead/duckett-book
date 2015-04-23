827f041a0ff92c82bee77d6c199bb4bbacd86247
Sara OC <saramoo@hotmail.com>
2015-04-23 23:05:20 +0100

    offer prices and expiry in a week

---
+++ c03/js/latestOffer.js
@@ -0,0 +1,47 @@
+(function() {
+
+var hotel = {
+  name: 'Park',
+  roomRate: 240,
+  discount: 15,
+  offerPrice: function(){
+    var offerRate = this.roomRate * ((100 - this.discount) / 100);
+    return offerRate;
+  }
+}
+
+var hotelName, roomRate, specialRate;
+
+hotelName = document.getElementById('hotelName');
+roomRate = document.getElementById('roomRate');
+specialRate = document.getElementById('specialRate');
+
+hotelName.textContent = hotel.name;
+roomRate.textContent = '$' + hotel.roomRate.toFixed(2);
+specialRate.textContent = '$' + hotel.offerPrice();
+
+var expiryMsg;
+var today;
+var elEnds;
+
+function offerExpires(today) {
+  var weekFromToday, day, date, month, year, dayNames, monthNames;
+  weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+  dayNames = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+  monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+
+  day = dayNames[weekFromToday.getDay()];
+  date = weekFromToday.getDate();
+  month = monthNames[weekFromToday.getMonth()];
+  year = weekFromToday.getFullYear();
+
+  expiryMsg = 'Offer expires next ';
+  expiryMsg += day + '<br />(' + date + ' ' + month + ' ' + year + ')';
+  return expiryMsg;
+}
+
+today = new Date();
+elEnds = document.getElementById('offerEnds');
+elEnds.innerHTML = offerExpires(today);
+
+}());
