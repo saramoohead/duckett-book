a01b0529213482467051b28ce5c3817c4e078b56
Sara OC <saramoo@hotmail.com>
2015-04-23 10:36:43 +0100

    variables

---
+++ c02/js/availability.js
@@ -0,0 +1,10 @@
+var inStock;
+var shipping;
+inStock = true;
+shipping = true;
+
+var elStock = document.getElementById('stock');
+elStock.className = inStock;
+
+var elShip = document.getElementById('shipping');
+elShip.className = shipping;
