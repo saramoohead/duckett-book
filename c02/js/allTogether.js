1b0f0d702c5691a8982815939f32f461de013993
Sara OC <saramoo@hotmail.com>
2015-04-23 12:25:21 +0100

    tidying up variables display

--- c02/js/allTogether.js
+++ c02/js/allTogether.js
@@ -1,4 +1,6 @@
+var greeting = 'Hello ';
 var firstName = 'Molly';
+var confirmationMessage = '. Please check your order:';
 var customSign = 'Montague House';
 var perTilePrice = 5;
 
@@ -7,8 +9,10 @@ var subTotal = totalTiles * perTilePrice;
 var shipping = subTotal * .10;
 var grandTotal = subTotal + shipping;
 
-var elName = document.getElementById('first_name');
-elName.textContent = firstName;
+var message = greeting + firstName + confirmationMessage;
+
+var elMessage = document.getElementById('greeting');
+elMessage.textContent = message;
 
 var elCustomSign = document.getElementById('custom_sign');
 elCustomSign.textContent = customSign;
@@ -17,10 +21,10 @@ var elTotalTiles = document.getElementById('total_tiles');
 elTotalTiles.textContent = totalTiles;
 
 var elSubTotal = document.getElementById('subtotal');
-elSubTotal.textContent = subTotal;
+elSubTotal.textContent = '$' + subTotal;
 
 var elShipping = document.getElementById('shipping');
-elShipping.textContent = shipping;
+elShipping.textContent = '$' + shipping;
 
 var elGrandTotal = document.getElementById('grand_total');
-elGrandTotal.textContent = grandTotal;
+elGrandTotal.textContent = '$' + grandTotal;
